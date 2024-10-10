5d85fa5abf5b81df1dee0cef1ff2ee079264a6d3
SabillillahX <sabillillahxtkj@gmail.com>
2024-10-10 21:45:57 +0700

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,14 @@
 import 'package:flutter/material.dart';
-import 'package:main/pages/register_page.dart';
+import 'package:get/get.dart';
+import 'app/routes/app_pages.dart';
 
 void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
+  runApp(
+    GetMaterialApp(
+      title: "Ticketing App",
+      initialRoute: AppPages.INITIAL,
+      getPages: AppPages.routes,
       debugShowCheckedModeBanner: false,
-      home: RegisterPage(),
-    );
-  }
+    ),
+  );
 }
--- lib/routes/routes_name.dart
+++
@@ -1,6 +0,0 @@
-import 'package:main/pages/register_page.dart';
-
-abstract class RoutesName {
-  static const RegisterPage = '/registerpage';
-  static const HomePage = '/homepage';
-}
