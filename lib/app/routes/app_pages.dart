b7b8ed7b164232a0b23549779c9cd153da94081e
SabillillahX <sabillillahxtkj@gmail.com>
2024-10-11 20:18:51 +0700

    big

--- lib/app/routes/app_pages.dart
+++ lib/app/routes/app_pages.dart
@@ -2,14 +2,6 @@ import 'package:get/get.dart';
 
 import '../../pages/aneh/bindings/aneh_binding.dart';
 import '../../pages/aneh/views/aneh_view.dart';
-import '../../pages/anjay/bindings/anjay_binding.dart';
-import '../../pages/anjay/views/anjay_view.dart';
-import '../../pages/home/bindings/home_binding.dart';
-import '../../pages/home/views/home_view.dart';
-import '../../pages/login/bindings/login_binding.dart';
-import '../../pages/login/views/login_view.dart';
-import '../../pages/profile/bindings/profile_binding.dart';
-import '../../pages/profile/views/profile_view.dart';
 
 part 'app_routes.dart';
 
@@ -20,29 +12,9 @@ class AppPages {
 
   static final routes = [
     GetPage(
-      name: _Paths.HOME,
-      page: () => const HomeView(),
-      binding: HomeBinding(),
-    ),
-    GetPage(
-      name: _Paths.LOGIN,
-      page: () => LoginView(),
-      binding: LoginBinding(),
-    ),
-    GetPage(
-      name: _Paths.PROFILE,
-      page: () => const ProfileView(),
-      binding: ProfileBinding(),
-    ),
-    GetPage(
       name: _Paths.ANEH,
       page: () => const AnehView(),
       binding: AnehBinding(),
-    ),
-    GetPage(
-      name: _Paths.ANJAY,
-      page: () => const AnjayView(),
-      binding: AnjayBinding(),
     ),
   ];
 }
