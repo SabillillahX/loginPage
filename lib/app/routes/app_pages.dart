e670d6f4838871e111ff97f11524fedcc2bf36ae
SabillillahX <sabillillahxtkj@gmail.com>
2024-10-11 19:52:47 +0700

    .

--- lib/app/routes/app_pages.dart
+++ lib/app/routes/app_pages.dart
@@ -4,6 +4,10 @@ import '../../pages/home/bindings/home_binding.dart';
 import '../../pages/home/views/home_view.dart';
 import '../../pages/login/bindings/login_binding.dart';
 import '../../pages/login/views/login_view.dart';
+import '../../pages/payment/bindings/payment_binding.dart';
+import '../../pages/payment/views/payment_view.dart';
+import '../../pages/profile/bindings/profile_binding.dart';
+import '../../pages/profile/views/profile_view.dart';
 
 part 'app_routes.dart';
 
@@ -22,6 +26,16 @@ class AppPages {
       name: _Paths.LOGIN,
       page: () => LoginView(),
       binding: LoginBinding(),
+    ),
+    GetPage(
+      name: _Paths.PROFILE,
+      page: () => const ProfileView(),
+      binding: ProfileBinding(),
+    ),
+    GetPage(
+      name: _Paths.PAYMENT,
+      page: () => const PaymentView(),
+      binding: PaymentBinding(),
     ),
   ];
 }
