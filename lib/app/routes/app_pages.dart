ce1cb53c661994ae06c77a7c6ddc73071773ccbf
SabillillahX <sabillillahxtkj@gmail.com>
2024-10-11 20:31:05 +0700

    kocak

--- lib/app/routes/app_pages.dart
+++ lib/app/routes/app_pages.dart
@@ -2,12 +2,6 @@ import 'package:get/get.dart';
 
 import '../../pages/home/bindings/home_binding.dart';
 import '../../pages/home/views/home_view.dart';
-import '../../pages/login/bindings/login_binding.dart';
-import '../../pages/login/views/login_view.dart';
-import '../../pages/payment/bindings/payment_binding.dart';
-import '../../pages/payment/views/payment_view.dart';
-import '../../pages/profile/bindings/profile_binding.dart';
-import '../../pages/profile/views/profile_view.dart';
 
 part 'app_routes.dart';
 
@@ -21,21 +15,6 @@ class AppPages {
       name: _Paths.HOME,
       page: () => const HomeView(),
       binding: HomeBinding(),
-    ),
-    GetPage(
-      name: _Paths.LOGIN,
-      page: () => LoginView(),
-      binding: LoginBinding(),
-    ),
-    GetPage(
-      name: _Paths.PROFILE,
-      page: () => const ProfileView(),
-      binding: ProfileBinding(),
-    ),
-    GetPage(
-      name: _Paths.PAYMENT,
-      page: () => const PaymentView(),
-      binding: PaymentBinding(),
     ),
   ];
 }
